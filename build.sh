2d7738c9f197f1031812610f2db2b67674f9c206
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2024-07-25 20:22:06 +0200

    first commit

---
+++ build.sh
@@ -0,0 +1,212 @@
+#!/bin/sh
+
+ANSI_RED="\033[1;31m"
+ANSI_GREEN="\033[1;32m"
+ANSI_YELLOW="\033[1;33m"
+ANSI_BLUE="\033[1;34m"
+ANSI_NORM="\033[0m"
+
+LOG_FILE="build.log"
+DOWNLOAD_AGENT="curl"
+BUILD_JOBS="$(nproc)"
+
+NP_BUILDDIR="$(dirname "$0")/"
+[ -z "$NP_BUILDDIR" ] && exit 2
+
+. "${NP_BUILDDIR}dl_build_install"
+
+error() {
+  printf "${ANSI_RED}error -> %s${ANSI_NORM}\n" "$1"
+  exit 1
+}
+
+warn() {
+  printf "${ANSI_YELLOW}warn -> %s${ANSI_NORM}\n" "$1"
+}
+
+info() {
+  printf "${ANSI_BLUE}-> %s${ANSI_NORM}\n" "$1"
+}
+
+success() {
+  printf "${ANSI_GREEN}-> %s${ANSI_NORM}\n" "$1"
+}
+
+check_installed() {
+  $1 > /dev/null 2>&1
+  [ "$?" -eq 127 ] && error "$1 command not installed / not found in \$PATH"
+}
+
+print_usage() {
+  echo "Neptunium Build Script by heliumhydride"
+  echo "usage: build.sh [OPTIONS]"
+  echo "options:"
+  echo "  -a, --arch [neptunium_arch]: architecture to build, amd64, x86, or arm64"
+  echo "  -j, --jobs [num. of jobs]:   use make with n jobs (default: number of logical cpus)"
+  echo "  -h, --help: shows this help"
+  echo "  -c, --clean: cleanup downloads, build files and output zips"
+  echo "  -v, --verbose: output commands to stdout, not log file (overrides -o)"
+  echo "  -o, --output-log [LOG_FILE]: output to log"
+  echo "  --x64dbg [custom_zip]: Use a custom-built version of x64dbg (must be same directory structure as in the snapshots of x64dbg)"
+  echo "  --no-prebuilt-llvm: Build llvm-mingw instead of pulling a binary (VERY LONG!)"
+  echo "  --free: use FOSS tools only (no dependency walker)"
+  echo "  --dl-agent [program]: use [program] to download files, default is curl (supported: curl,wget)"
+  echo ""
+  echo "you can edit ${NP_BUILDDIR}dl_build_install.sh to change URLs of downloads, build flags, etc..."
+  echo "NOTE: arm64 building is UNTESTED"
+  exit 1
+}
+
+
+[ -z "$*" ] && print_usage
+while :; do
+  case "$1" in
+    -a|--arch) shift; ARCH=$1;;
+    -j|--jobs) shift; BUILD_JOBS=$1;;
+    --no-prebuilt-llvm) BUILD_LLVM=1;;
+    --x64dbg) shift; X64DBG_CUSTOM_PATH=$1;;
+    --dl-agent) shift; DOWNLOAD_AGENT="$1";;
+    --free) FREE_SOFTWARE_ONLY=1;;
+    -o|--output-log) shift; LOG_FILE="$1";;
+    -v|--verbose) LOG_FILE='';;
+    -c|--clean) _clean_mode=1;;
+    --) shift; break;;
+    '') break;;
+    *) print_usage;;
+  esac
+  shift
+done
+
+if [ "$_clean_mode" = 1 ]; then
+  info "cleaning all inside build directory and downloads"
+  clean_fail=0
+  rm -r "${NP_BUILDDIR}build"/* || warn "something wrong happened while cleaning build files"; _clean_fail=1
+  rm -r "${NP_BUILDDIR}download"/* || warn "something wrong happened while cleaning downloads"; _clean_fail=1
+  rm -r "${NP_BUILDDIR}output"/* || warn "something wrong happened while cleaning output zips" _clean_fail=1
+  exit "$clean_fail"
+fi
+
+check_installed "$DOWNLOAD_AGENT"
+check_installed tar
+check_installed gzip
+check_installed xz
+check_installed unzip
+check_installed 7z
+
+
+case "$ARCH" in
+  amd64) TARGET_HOST="x86_64-w64-mingw32"
+         BUILD_PREFIX="/neptunium64";;
+  x86)   TARGET_HOST="i686-w64-mingw32"
+         BUILD_PREFIX="/neptunium32";;
+  arm64) TARGET_HOST="aarch64-w64-mingw32"
+         BUILD_PREFIX="/neptuniumarm64";;
+esac
+
+check_installed "$TARGET_HOST"-gcc
+check_installed "$TARGET_HOST"-g++
+
+case "$ARCH" in
+  amd64|x86|arm64);;
+  *) error "unsupported architecture $ARCH";;
+esac
+
+case "$DOWNLOAD_AGENT" in
+  curl) _dl_cmd="curl -LJO";;
+  wget) _dl_cmd="wget";;
+  *) error "unsupported download agent $DOWNLOAD_AGENT";;
+esac
+
+echo
+echo "${ANSI_BLUE}arch:                   ${ANSI_GREEN}${ARCH}${ANSI_NORM}"
+echo "${ANSI_BLUE}download agent:         ${ANSI_GREEN}${DOWNLOAD_AGENT}${ANSI_NORM}"
+
+if [ "$BUILD_LLVM" = 1 ]; then echo "${ANSI_BLUE}build llvm-mingw:       ${ANSI_GREEN}yes${ANSI_NORM}"
+else echo "${ANSI_BLUE}build llvm-mingw:       ${ANSI_RED}no${ANSI_NORM}"
+fi
+
+if [ "$FREE_SOFTWARE_ONLY" = 1 ]; then echo "${ANSI_BLUE}free software only:     ${ANSI_GREEN}yes${ANSI_NORM}"
+else echo "${ANSI_BLUE}free software only:     ${ANSI_RED}no${ANSI_NORM}"
+fi
+
+[ -n "$X64DBG_CUSTOM_PATH" ] && echo "${ANSI_BLUE}x64dbg custom zip path: ${ANSI_GREEN}${X64DBG_CUSTOM_PATH}${ANSI_NORM}"
+
+echo "Proceed with these parameters ? (Y/n)"
+read -r _proceed
+case "$_proceed" in [nN]) exit 0;; esac
+
+
+info "ok, downloading needed files"
+
+# ---DOWNLOADING---
+# base system tools
+download_busybox_w32
+download_libarchive
+download_curl
+download_file
+download_conemu
+# development tools
+download_llvm
+download_nasm
+download_gmake
+download_vim
+download_w64devkit
+[ -z "$X64DBG_CUSTOM_PATH" ] && download_x64dbg
+[ "$FREE_SOFTWARE_ONLY" = 1 ] || download_depends
+# additional libs
+download_pdcurses
+download_host_libressl
+download_host_libgnurx
+
+info "creating base directory structure"
+# create base directory structure
+mkdir -pv "$NP_BUILDDIR"/install_dir/"$BUILD_PREFIX"/bin
+mkdir -pv "$NP_BUILDDIR"/install_dir/"$BUILD_PREFIX"/etc
+mkdir -pv "$NP_BUILDDIR"/install_dir/"$BUILD_PREFIX"/home
+
+# --- COMPILING HOST LIBS ---
+build_pdcurses
+build_host_libressl
+build_host_libgnurx
+
+# --- COMPILING NEPTUNUM DISTRIBUTION ---
+build_busybox_w32
+build_libarchive
+build_curl
+build_file
+build_conemu
+build_nasm
+build_gmake
+build_vim
+# 3 following pkgs from w64devkit
+build_pkg_config
+build_vc++filt
+build_debugbreak
+#build_x64dbg
+build_llvm
+
+# --- INSTALLING ---
+install_busybox_w32
+install_libarchive
+install_libressl_libs
+install_curl
+install_libgnurx_libs
+install_file
+install_conemu
+[ "$BUILD_LLVM" = 1 ] || install_llvm
+install_nasm
+install_gmake
+install_pdcurses
+install_vim
+# 3 following pkgs from w64devkit
+install_pkg_config
+install_vc++filt
+install_debugbreak
+install_x64dbg
+[ "$FREE_SOFTWARE_ONLY" = 1 ] || install_depends
+
+info "creating distribution zip"
+ZIPNAME="neptunium-$ARCH-$(date +%Y.%m.%d).7z"
+
+7z a -mx7 -r "$NP_BUILDDIR"/output/"$ZIPNAME" "$NP_BUILDDIR"/install_dir/* || error "creating distribution zip failed"
+success "enjoy your new neptunium $ARCH build at $NP_BUILDDIR/output/$ZIPNAME !"
---
+++ dl_build_install.sh
@@ -0,0 +1,305 @@
+#!/bin/sh
+
+CONEMU_URL="https://github.com/Maximus5/ConEmu/archive/refs/tags/v23.07.24.tar.gz"
+NASM_URL="https://www.nasm.us/pub/nasm/stable/nasm-2.16.03.tar.xz"
+MAKE_URL="https://ftpmirror.gnu.org/make/make-4.4.1.tar.gz"
+BUSYBOX_URL="https://github.com/rmyorston/busybox-w32/archive/refs/tags/FRP-5398-g89ae34445.tar.gz"
+VIM_URL="https://www.vim.org/downloads/vim-9.1.tar.bz2"
+FILE_URL="http://ftp.astron.com/pub/file/file-5.45.tar.gz"
+LIBARCHIVE_URL="https://www.libarchive.de/downloads/libarchive-3.7.4.tar.xz"
+CURL_URL="https://curl.se/download/curl-8.8.0.tar.xz"
+W64DEVKIT_URL="https://github.com/skeeto/w64devkit/archive/refs/tags/v1.23.0.tar.gz"
+PDCURSES_URL="https://github.com/wmcbrine/PDCurses/archive/refs/tags/3.9.tar.gz"
+NEPTUNIUM_BASE_URL=""
+
+LIBRESSL_URL="https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.9.2.tar.gz"
+LIBGNURX_URL="https://downloads.sourceforge.net/mingw/Other/UserContributed/regex/mingw-regex-2.5.1/mingw-libgnurx-2.5.1-src.tar.gz"
+
+DEPENDS_X86_URL="https://www.dependencywalker.com/depends22_x86.zip"
+DEPENDS_AMD64_URL="https://www.dependencywalker.com/depends22_x64.zip"
+
+X64DBG_URL="https://downloads.sourceforge.net/project/x64dbg/snapshots/snapshot_2024-07-21_20-36.zip"
+
+LLVM_MINGW_SRC_URL="https://github.com/mstorsjo/llvm-mingw/archive/refs/tags/20240619.tar.gz"
+LLVM_MINGW_BIN_AMD64_URL="https://github.com/mstorsjo/llvm-mingw/releases/download/20240619/llvm-mingw-20240619-msvcrt-x86_64.zip"
+LLVM_MINGW_BIN_X86_URL="https://github.com/mstorsjo/llvm-mingw/releases/download/20240619/llvm-mingw-20240619-msvcrt-i686.zip"
+LLVM_MINGW_BIN_ARM64_URL="https://github.com/mstorsjo/llvm-mingw/releases/download/20240619/llvm-mingw-20240619-ucrt-aarch64.zip"
+
+# neptunium base files
+download_neptunium_base {
+  cd "$NP_BUILDDIR"/download || error "directory error"
+  $_dl_cmd "$NEPTUNIUM_BASE_URL" || exit 1
+  tar zxvf "$NP_BUILDDIR"/download/neptunium-base-files-*.tar.gz -C "$NP_BUILDDIR"/build
+  mv "$NP_BUILDDIR"/build/neptunium-base-files-* "$NP_BUILDDIR"/build/neptunium-base-files
+}
+
+install_neptunium_base {
+  cp -rv "$NP_BUILDDIR"/build/neptunium-base-files/common/*  "$NP_BUILDDIR"/install_dir/"$BUILD_PREFIX"/
+  cp -rv "$NP_BUILDDIR"/build/neptunium-base-files/"$ARCH"/* "$NP_BUILDDIR"/install_dir/"$BUILD_PREFIX"/
+}
+
+# busybox-w32 (https://frippery.org/busybox-w32)
+download_busybox_w32 {
+  cd "$NP_BUILDDIR"/download || error "directory error"
+  $_dl_cmd "$BUSYBOX_URL" || exit 1
+  tar zxvf "$NP_BUILDDIR"/download/busybox-w32-FRP-*.tar.gz -C "$NP_BUILDDIR"/build || exit 1
+  mv "$NP_BUILDDIR"/build/busybox-w32-FRP-* "$NP_BUILDDIR"/busybox-w32 || exit 1
+}
+
+build_busybox_w32 {
+  cd "$NP_BUILDDIR/build/busybox-w32" || error "directory error"
+  CROSS_COMPILE="${TARGET_HOST}-"
+  case $ARCH in
+    amd64) make mingw64_defconfig;;
+    x86)   make mingw32_defconfig;;
+    arm64) make mingw64a_defconfig;;
+  esac
+  # patch config accordingly to neptunium64_config
+  make -j${BUILD_JOBS}
+}
+
+install_busybox_w32 {
+  # build aliases.c from w64devkit
+  cd "$NP_BUILDDIR"/build/busybox-w32 || error "directory error"
+  cp busybox.exe -v "$NP_BUILDDIR"/install_dir/"$BUILD_PREFIX"/bin/ || exit 1
+}
+
+# libarchive (bsdcpio, bsdtar)
+download_libarchive {
+  cd "$NP_BUILDDIR/download" || exit 1
+  $_dl_cmd "$LIBARCHIVE_URL" || exit 1
+  tar Jxvf "$NP_BUILDDIR"/download/libarchive-*.tar.xz -C "$NP_BUILDDIR"/build || exit 1
+  mv "$NP_BUILDDIR"/libarchive-* "$NP_BUILDDIR"/libarchive || exit 1
+}
+
+build_libarchive {
+
+}
+
+install_libarchive {
+
+}
+
+# Libressl (required for Curl)
+download_host_libressl {
+  
+}
+
+build_host_libressl {
+  
+}
+
+install_host_libressl {
+  
+}
+
+install_libressl_libs{
+  
+}
+
+# Curl
+download_curl {
+
+}
+
+build_curl {
+
+}
+
+install_curl {
+
+}
+
+# libgnurx (required for File)
+download_host_libgnurx {
+  
+}
+
+build_host_libgnurx {
+  
+}
+
+install_host_libgnurx {
+  
+}
+
+install_libgnurx_libs {
+
+}
+
+# File
+download_file {
+
+}
+
+build_file {
+
+}
+
+install_file {
+
+}
+
+# ConEmu
+download_conemu {
+
+}
+
+build_conemu {
+  cd "$BP_BUILDDIR"/build/conemu/src
+  CROSS_HOST="${TARGET_HOST}-" make -j12 -f makefile_all_gcc WIDE=y
+}
+
+install_conemu {
+
+}
+
+# LLVM-MinGW (http://github.com/mstorsjo/llvm-mingw)
+download_llvm {
+  cd "$NP_BUILDDIR"/download || error "directory error"  
+  if [ "$BUILD_LLVM" = 1 ]; then
+    $_dl_cmd "$LLVM_MINGW_SRC_URL"
+    tar zxvf "$NP_BUILDDIR"/download/llvm-mingw-*.tar.gz
+  else
+    case "$ARCH" in
+      amd64) $_dl_cmd "$LLVM_MINGW_BIN_AMD64_URL";;
+      x86) $_dl_cmd "$LLVM_MINGW_BIN_X86_URL";;
+      arm64) $_dl_cmd "$LLVM_MINGW_BIN_ARM64_URL";;
+    esac
+    unzip -d "$NP_BUILDDIR"/build "$NP_BUILDDIR"/download/llvm-mingw-*.zip
+  fi
+  mv -v "$NP_BUILDDIR"/build/llvm-mingw-* "$NP_BUILDDIR"/build/llvm-mingw
+}
+
+build_llvm {
+  cd "$NP_BUILDDIR"/llvm-mingw || error "directory error"
+  # also installs llvm-mingw in the process, which is quite handy
+  ./build-all.sh --host="$TARGET_HOST" "$NP_BUILDDIR"/install_dir/"$BUILD_PREFIX"
+}
+
+install_llvm {
+  [ "$BUILD_LLVM" = 1 ] || {
+    cp -rv "$NP_BUILDDIR"/build/llvm-mingw/* "$NP_BUILDDIR"/install_dir/"$BUILD_PREFIX"
+  }
+}
+
+# Netwide assembler
+download_nasm {
+  cd "$NP_BUILDDIR"/download || error "directory error"
+  $_dl_cmd "$NASM_URL" || exit 1
+  tar Jxvf "$NP_BUILDDIR"/download/nasm-*.tar.xz -C "$NP_BUILDDIR"/build
+  mv -v "$NP_BUILDDIR"/build/nasm-* "$NP_BUILDDIR"/build/nasm
+}
+
+build_nasm {
+  ./configure --host="$TARGET_HOST" --prefix="$BUILD_PREFIX"
+  make -j${BUILD_JOBS}
+}
+
+install_nasm {
+  cd "$NP_BUILDDIR"/build/nasm || error "directory error"
+  cp -v nasm.exe ndisasm.exe "$NP_BUILDDIR"/install_dur/"$BUILD_PREFIX"/bin
+}
+
+# GNU Make
+download_gmake {
+  cd "$NP_BUILDDIR"/download || error "directory error"
+  $_dl_cmd "$MAKE_URL" || exit 1
+  tar zxvf "$NP_BUILDDIR"/download/make-*.tar.gz -C "$NP_BUILDDIR"/build
+  mv -v "$NP_BUILDDIR"/build/make-* "$NP_BUILDDIR"/build/make
+}
+
+build_gmake {
+  cd "$NP_BUILDDIR"/build/make || error "directory error"
+  ./configure --disable-nls --host="$TARGET_HOST" --prefix="$BUILD_PREFIX"
+  make -j${BUILD_JOBS}
+}
+
+install_gmake {
+  cd "$NP_BUILDDIR"/build/make || error "directory error"
+  make install DESTDIR="$NP_BUILDDIR/install_dir"
+}
+
+# Vim
+download_vim {
+
+}
+
+build_vim {
+
+}
+
+install_vim {
+
+}
+
+# pkg-config, vc++filt, debugbreak from w64devkit
+download_w64devkit {
+  cd "$NP_BUILDDIR"
+  $_dl_cmd "$W64DEVKIT_URL"  || error "downloading w64devkit failed"
+  tar zxvf "$NP_BUILDDIR"/download/w64devkit-*.tar.gz -C "$NP_BUILDDIR"/build
+  mv "$NP_BUILDDIR"/build/w64devkit-* "$NP_BUILDDIR"/build/w64devkit
+}
+
+build_pkg_config {
+  cd "$NP_BUILDDIR"/build/w64devkit/src || error "directory error"
+  ${TARGET_HOST}-gcc -Os -fno-asynchronous-unwind-tables -fno-builtin -Wl,--gc-sections \
+        -s -,pstdmob -DPKG_CONFIG_PREFIX="\"/$ARCH\"" -o pkg-config.exe pkg-config.c -lkernel32
+}
+
+build_vc++filt {
+  cd "$NP_BUILDDIR"/build/w64devkit/src || error "directory error"
+  ${TARGET_HOST}-gcc -Os -fno-asynchronous-unwind-tables -fno-builtin -Wl,--gc-sections \
+        -s -nostdlib -o vc++filt.exe vc++filt.c -lkernel32 -lshell32 -ldbghelp
+}
+
+
+build_debugbreak {
+  cd "$NP_BUILDDIR"/build/w64devkit/src || error "directory error"
+  ${TARGET_HOST}-gcc -Os -fno-asynchronous-unwind-tables -Wl,--gc-sections -s -nostdlib \
+        -o debugbreak.exe debugbreak.c -lkernel32
+}
+
+install_pkg_config {
+  cp -v "$NP_BUILDDIR"/build/w64devkit/src/pkg-config.exe "$NP_BUILDDIR"/install_dir/$BUILD_PREFIX/bin/
+}
+
+install_vc++filt {
+  cp -v "$NP_BUILDDIR"/build/w64devkit/src/vc++filt.exe "$NP_BUILDDIR"/install_dir/$BUILD_PREFIX/bin/
+}
+
+install_debugbreak {
+  cp -v "$NP_BUILDDIR"/build/w64devkit/src/debugbreak.exe "$NP_BUILDDIR"/install_dir/$BUILD_PREFIX/bin/
+}
+
+# PDCurses
+download_pdcurses {
+
+}
+
+build_pdcurses {
+
+}
+
+install_pdcurses {
+
+}
+
+# x64dbg
+download_x64dbg {
+
+}
+
+install_x64dbg {
+
+}
+
+# Dependency walker (depends.exe)
+download_depends {
+
+}
+
+install_depends {
+
+}
+
