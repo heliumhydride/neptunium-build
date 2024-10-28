421a54757102258d8f7cef5c0c88daf2f6623cc9
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2024-10-28 19:17:20 +0100

    cleanup additional directories

--- build.sh
+++ build.sh
@@ -84,8 +84,10 @@ done
   clean_fail=0
   _rm_v=""
   [ "$VERBOSE" = 1 ] && _rm_v="-v"
-  rm -r $_rm_v "$NP_BUILDDIR"build/* || warn "something wrong happened while cleaning build files"; _clean_fail=1
   rm -r $_rm_v "$NP_BUILDDIR"download/* || warn "something wrong happened while cleaning downloads"; _clean_fail=1
+  rm -r $_rm_v "$NP_BUILDDIR"build/* || warn "something wrong happened while cleaning build files"; _clean_fail=1
+  rm -r $_rm_v "$NP_BUILDDIR"host/* || warn "something wrong happened while cleaning the host toolchain libraries"; _clean_fail=1
+  rm -r $_rm_v "$NP_BUILDDIR"install_dir/* || warn "something wrong happened while cleaning the neptunium temp install directory"; _clean_fail=1
   rm -r $_rm_v "$NP_BUILDDIR"output/* || warn "something wrong happened while cleaning output zips"; _clean_fail=1
   exit "$clean_fail"
 }
